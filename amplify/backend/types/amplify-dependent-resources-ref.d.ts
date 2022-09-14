b7c59310b9c61aaf4c66a2972800e68e20472b8b
Nicholas Wayoe <nwayoe@yahoo.com>
2022-09-14 14:31:00 -0400

    Added Storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "notesapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "store1": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
